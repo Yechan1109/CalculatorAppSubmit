83363edc925c5b2ccd49f10066ca96932f2c8572
ychic1109-DA <ychic1109@gmail.com>
2024-06-30 20:10:29 +0900

    Last Updated

--- CalculatorAppSubmit/Views/ViewController.swift
+++ CalculatorAppSubmit/Views/ViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    // MARK: -
     private let buttonNames = [
         ["7", "8", "9", "+"], // 각 array는 row, row 안의 개별 값은 value
         ["4", "5", "6", "-"],
@@ -9,13 +11,13 @@ class ViewController: UIViewController {
         ["AC", "0", "=", "/"]
     ]
     
-    private var currentExpression: String = ""
-
-    // 계산기 동작 view label
+    private var currentValue: String = ""
+    
+    // 계산기 동작 Outputlabel
     private let outputLabel: UILabel = {
         let label = UILabel()
         label.text = "0"
-        label.textColor = UIColor(hexCode: "#ffffff")
+        label.textColor = UIColor(hexCode: "#ffffff") // extension UIColor로 헥스코드 사용
         label.textAlignment = .right
         label.font = UIFont.boldSystemFont(ofSize: 60)
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -43,26 +45,28 @@ class ViewController: UIViewController {
     }
     
     
-//          오토레이아웃 설정
+    // MARK: - 레이아웃 func
+    
+    // 오토레이아웃
     private func configureUI() {
         view.backgroundColor = .black
-//        view.addSubview(outputLabel) + view.addSubview(hstackView) -> foreach로 각각 할당
+        //        view.addSubview(outputLabel) + view.addSubview(hstackView) -> foreach로 각각 할당
         [outputLabel, vstackView].forEach { view.addSubview($0) }
-
+        
         NSLayoutConstraint.activate([
             outputLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
             outputLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
             outputLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
             outputLabel.heightAnchor.constraint(equalToConstant: 100),
             
-            vstackView.widthAnchor.constraint(equalToConstant: 350),
+            vstackView.widthAnchor.constraint(equalToConstant: 350), // 버튼 크기 80, spacing 10 -> (80*4) + (10*3) = 350
             vstackView.heightAnchor.constraint(equalToConstant: 350),
             vstackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             vstackView.topAnchor.constraint(equalTo: outputLabel.bottomAnchor, constant: 60),
         ])
     }
-
-    // 버튼 및 stackview 교차 관련 세팅
+    
+    // 버튼 및 horizon stackview 설정
     private func setButtons() {
         for row in buttonNames {
             let hstackView = UIStackView()
@@ -76,7 +80,7 @@ class ViewController: UIViewController {
                 button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
                 // 버튼 기능별 컬러값 주기
                 button.layer.cornerRadius = 40
-                if ["+", "-", "*", "/", "="].contains(value) {
+                if ["+", "-", "*", "/", "=", "AC"].contains(value) {
                     button.backgroundColor = UIColor(hexCode: "#FE5A1D")
                 } else {
                     button.backgroundColor = UIColor(hexCode: "#808080")
@@ -91,51 +95,54 @@ class ViewController: UIViewController {
     }
     
     
+    // MARK: - 버튼 클릭 동작 기능 func
+    @objc private func buttonTapped(_ sender: UIButton) {
+        guard let getTitle = sender.currentTitle else { return }
         
-        @objc private func buttonTapped(_ sender: UIButton) {
-                guard let getTitle = sender.currentTitle else { return }
-                
-                switch getTitle {
-                case "0"..."9", "+", "-", "*", "/":
-                    if currentExpression == "0" {
-                        currentExpression = getTitle
-                    } else {
-                        currentExpression += getTitle
-                    }
-                    outputLabel.text = currentExpression
-                case "=":
-                    actOperation()
-                case "AC":
-                    clear()
-                default:
-                    break
-                }
+        // switch문으로 기능 분할
+        switch getTitle {
+        case "0"..."9", "+", "-", "*", "/":
+            if currentValue == "0" {        // 처음 0값시 표기 삭제
+                currentValue = getTitle
+            } else {
+                currentValue += getTitle
             }
+            outputLabel.text = currentValue
+        case "=":
+            resultOperation()
+        case "AC":
+            clear()
+        default:
+            break
+        }
+    }
     
-        private func actOperation() {
-            if let result = calculate(expression: currentExpression) {
-                outputLabel.text = "\(result)"
-                currentExpression = "\(result)"
-            } else {
-                currentExpression = "0"
-            }
+    // 초기화 버튼(AC) 구현
+    private func clear() {
+        currentValue = "0"
+        outputLabel.text = "0"
+    }
+    
+    // "=" 연산 수행 구현
+    private func resultOperation() {
+        if let result = calculate(currentValue) {
+            outputLabel.text = "\(result)"
+            currentValue = "\(result)"
+        } else {
+            currentValue = "0"
         }
-        
-        private func clear() {
-            currentExpression = "0"
-            outputLabel.text = "0"
+    }
+    
+    // 값과 수식을 계산하는 NSExpression 사용
+    private func calculate(_ expression: String) -> Int? {
+        let expression = NSExpression(format: expression)
+        if let result = expression.expressionValue(with: nil, context: nil) as? Int {
+            return result
+        } else {
+            return nil
         }
-        
-        // 수식 문자열을 넣으면 계산해주는 메서드
-        private func calculate(expression: String) -> Int? {
-            let expression = NSExpression(format: expression)
-            if let result = expression.expressionValue(with: nil, context: nil) as? Int {
-                return result
-            } else {
-                return nil
-            }
-        }
-
+    }
+    
 }
 
 
@@ -160,8 +167,7 @@ extension UIColor {
                   alpha: alpha)
     }
 }
-
-//@available(iOS 17.0, *)
+// MARK: -
 #Preview {
     ViewController()
 }
